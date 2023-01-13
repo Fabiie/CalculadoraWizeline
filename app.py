f87429ea829602e173f2193a682a8e505b87a022
Fabiola Jiménez <jmnz.c.f@gmail.com>
2023-01-13 13:27:23 -0600

    Creación menú

---
+++ app.py
@@ -0,0 +1,47 @@
+def main():
+    opciones = ["Salir", "SUMA", "RESTA", "MULTIPLICACION", "DIVISION", "RAIZ", "EXPONENTE", "SENO", "COSENO", "TANGENTE"]
+    opcion = menu()
+
+    while opcion != "0":
+        print("Bienvenido a la operación %s" %opciones[int(opcion)])
+        if opcion == "1":
+            suma()
+        if opcion == "2":
+            resta()
+        if opcion == "3":
+            multi()
+        if opcion == "4":
+            division()
+        if opcion == "5":
+            raiz()
+        if opcion == "6":
+            exp()
+        if opcion == "7":
+            sen()
+        if opcion == "8":
+            cos()
+        if opcion == "9":
+            tan()
+        opcion = menu()
+
+    print("Saliendo...")
+
+
+def menu():
+    respuesta = input(""" Calculadora
+    Seleccione una de las siguientes opciones:
+    1.- Sumar
+    2.- Restar
+    3.- Multiplicar
+    4.- Dividir
+    5.- Raiz n
+    6.- Exponente n
+    7.- Seno
+    8.- Coseno
+    9.- Tangente
+    0.- Salir  
+    """ )
+    return respuesta
+
+if __name__ == '__main__':
+        main()
